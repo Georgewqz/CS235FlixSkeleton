60a1fbe6ff0a4c1487a0a11f1ebc1437680a37e6
Georgewqz <your>
2020-08-31 10:46:28 +1200

    updated

--- datafilereaders/movie_file_csv_reader.py
+++ datafilereaders/movie_file_csv_reader.py
@@ -6,17 +6,47 @@ from domainmodel.genre import Genre
 from domainmodel.director import Director
 
 class MovieFileCSVReader:
-
     def __init__(self, file_name: str):
         self.__file_name = file_name
+        self.__dataset_of_movies = []
+        self.__dataset_of_actors = []
+        self.__dataset_of_directors = []
+        self.__dataset_of_genres = []
 
     def read_csv_file(self):
         with open(self.__file_name, mode='r', encoding='utf-8-sig') as csvfile:
             movie_file_reader = csv.DictReader(csvfile)
 
-            index = 0
             for row in movie_file_reader:
                 title = row['Title']
                 release_year = int(row['Year'])
-                print(f"Movie {index} with title: {title}, release year {release_year}")
-                index += 1
+                movie = Movie(title, release_year)
+                director = Director(row["Director"])
+                actor = row['Actors'].split(",")
+                genre = row['Genre'].split(",")
+                
+                self.__dataset_of_movies.append(movie)
+                
+                if director not in self.__dataset_of_directors:
+                    self.__dataset_of_directors.append(director)
+                    
+                for a in actor:
+                    if Actor(a) not in self.__dataset_of_actors:
+                        self.__dataset_of_actors.append(Actor(a))
+                        
+                for g in genre:
+                    if Genre(g) not in self.__dataset_of_genres:
+                        self.__dataset_of_genres.append(Genre(g))
+    
+    @property         
+    def dataset_of_movies(self):
+        return self.__dataset_of_movies
+    @property
+    def dataset_of_actors(self):
+        return self.__dataset_of_actors
+    @property
+    def dataset_of_directors(self):
+        return self.__dataset_of_directors
+    @property
+    def dataset_of_genres(self):
+        return self.__dataset_of_genres
